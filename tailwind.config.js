e64da772ba814cf701a15b8e5484fbcc906441a9
vanajmoorthy <vanajmoorthy@gmail.com>
2020-01-07 07:30:57 +0530

    homepage done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      kbold: ['Kollektif-Bold', 'sans-serif'],
+      kitalic: ['Kollektif-Italic', 'sans-serif'],
+      k: ['Kollektif', 'sans-serif'],
+      kbolditalic: ['Kollektif-BoldItalic', 'sans-serif']
+  },
     extend: {}
   },
   variants: {},
